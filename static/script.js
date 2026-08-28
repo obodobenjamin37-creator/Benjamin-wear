e7979fc890fbd0fd51987013389e657c5cacda07
obodobenjamin37-creator <obodobenjamin37@gmail.com>
2026-08-28 16:31:08 +0100

    Added smooth scrolling and fixed navigation

--- static/script.js
+++ static/script.js
@@ -477,3 +477,15 @@ window.addEventListener('load', function() {
         loadProductsForDelete();
     }
 });
+
+// Smooth scroll for Navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+            e.preventDefault();
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
